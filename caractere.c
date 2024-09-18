cbc5610d8184fee9e3a3b94de77b11f652b143f4
ÍTALO RODRIGUES NERI <italo.neri@a.ucb.br>
2024-09-18 09:13:41 -0300

    terceira versão do código
    alterado erro no arquivo caractere.c

--- caractere.c
+++ caractere.c
@@ -7,5 +7,5 @@ int f(int x, int b){
 int a0 = 186.752, a1 = -148.235, a2 = 34.5049, a3 = -3.5091, a4 = 0.183166;
 int a5 = -0.00513554, a6 = 0.0000735464, a7 = -4.22038*pow(10,-7);
 
-return a0 + (a1 + b)*x + pow(a2*x,2) + pow(a3*x,3) + pow(a4*x,4) + pow(a5*x,5) + pow(a6*x,6) + pow(a7*x,7);
+return a0 + (a1 + b)*x + a2* pow(x,2) + a3* pow(x,3) + a4* pow(x,4) + a5* pow(x,5) + a6* pow(x,6) + a7* pow(x,7);
 }
