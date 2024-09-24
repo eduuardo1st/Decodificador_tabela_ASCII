83c734a9a5f0dbd8c1dd5aa2d2486142b97883d9
Eduardo Gonçalves <eduardouc388@gmail.com>
2024-09-24 17:26:57 -0300

    Oitava versão do código
    Aqui o código está quase completo, ele está com um erro na execução.

--- decodificador.c
+++ decodificador.c
@@ -3,29 +3,39 @@
 #include <math.h>
 #include <string.h>
 #include <stdlib.h>
-#include "caractere.h"
-#define mensagem 101
+double f(int x, int b);
+double f(int x, int b){
+    int i;
+double a0 = 186.752, a1 = -148.235, a2 = 34.5049, a3 = -3.5091, a4 = 0.183166;
+double a5 = -0.00513554, a6 = 0.0000735464, a7 = -4.22038*pow(10,-7);
+
+return round(a0 + (a1 + b)*x + a2* pow(x,2) + a3* pow(x,3) + a4* pow(x,4) + a5* pow(x,5) + a6* pow(x,6) + a7* pow(x,7));
+}
 int main(){
-     int numMensagens, a, b, i, x = 1;
-     char mensagemHexa[101], aux[101];
+     int numMensagens, a, b, i, j, x;
+     char mensagemHexa[101], aux[3];
      //coleta de informacoes
      printf("Digite o numero de mensagens interceptadas: ");
      scanf("%d", &numMensagens);
-     fflush(stdin);
+     while (getchar() != '\n');
 
      //repetir a execucao ate alcancar o numero de mensagens
      for(i=0; i<numMensagens; i++){
         printf("digite o valor de b: ");
         scanf("%d", &b);
-        fflush(stdin);
+        while (getchar() != '\n');
+
         printf("digite a mensagem decodificada: ");
-        fgets(mensagemHexa, mensagem, stdin);
-        fflush(stdin);
+        fgets(mensagemHexa, 101, stdin);
+        mensagemHexa[strcspn(mensagemHexa, "\n")] = '\0';
+        
+        x=1;
         //o codigo abaixo eh para decodificar a mensagem
-        for(i=0; i<100; i+=2){
-            aux[0] = mensagemHexa[i];
-            aux[1] = mensagemHexa[i+1];
-            a = strtol(aux[1], stdin, 16);
+        for(j=0; j<100; j+=2){
+            aux[0] = mensagemHexa[j];
+            aux[1] = mensagemHexa[j+1];
+            aux[2] = '\0';
+            a = strtol(aux, NULL, 16);
             if(a == 0){
                 break;
             }
