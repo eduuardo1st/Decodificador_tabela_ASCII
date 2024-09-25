3d4a830cb1fcc1627f351e4fccd9c06ccff33907
Eduardo Gonçalves <eduardouc388@gmail.com>
2024-09-25 14:05:00 -0300

    limite versão 3
    aqui o código foi melhorado e com alguns ajustes estará pronto.

--- Limite.c
+++ Limite.c
@@ -5,18 +5,18 @@ int main () {
     
 
 printf("Tabela de limites dos tipos de variaveis inteiras:\n");
-    printf("------------------------------------------------------------------------------------\n");
-    printf("| TIPO   |   MINIMO   |   MAXIMO  | \n");
-    printf("------------------------------------------------------------------------------------\n");
+printf("-------------------------------------------------------\n");
+printf("| TIPO   |        MINIMO        |       MAXIMO        | \n");
+printf("-------------------------------------------------------\n");
 
-printf("| char |  %d  |  %d   | \n", CHAR_MIN, CHAR_MAX);
-printf("| int  | %d   | %d    | \n", INT_MIN, INT_MAX);
-printf("| shrt | %d   | %d    | \n", SHRT_MIN, SHRT_MAX);
-printf("| uint | %5u | %15u      |\n", 0 , UINT_MAX);
-printf("| long | %ld | %ld |\n", LONG_MIN, LONG_MAX);
-printf("| ulong | %u | %u |\n", 0, ULONG_MAX);
-printf("| char | %d | %d |\n", CHAR_MIN, CHAR_MAX);
-printf("| char | %d | %d |\n", CHAR_MIN, CHAR_MAX);
+printf("| char   |         %d         |         %d         | \n", CHAR_MIN, CHAR_MAX);
+printf("| int    |      %d     |      %d     | \n", INT_MIN, INT_MAX);
+printf("| shrt   |        %d        |        %d        | \n", SHRT_MIN, SHRT_MAX);
+printf("| uint   | %5u | %15u      |\n", 0 , UINT_MAX);
+printf("| long   | %ld | %ld |\n", LONG_MIN, LONG_MAX);
+printf("| ulong  | %u | %u |\n", 0, ULONG_MAX);
+printf("| llong  | %lld | %lld |\n", LLONG_MIN, LLONG_MAX);
+printf("| ullong | %u | %llu |\n", 0, ULLONG_MAX);
 
 
 
